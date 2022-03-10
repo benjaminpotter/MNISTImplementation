c85ca77c6ddef4cb924900b50b8ebf8ec00c8e89
Adam <monsieurfencer@gmail.com>
2022-03-10 15:18:30 -0500

    added mind
    he lives

--- website/src/predictor/main.py
+++ website/src/predictor/main.py
@@ -1,19 +1,26 @@
 from flask import Flask, request
 from json import loads, dumps
+import numpy as np
+import tensorflow as tf
 
 from random import randint # TEMPORARY
+model = tf.keras.models.load_model('./digitClassifier.model')
+
 
 app = Flask(__name__)
 
 @app.route("/predict", methods=["GET", "POST"])
 def predict():
+  
+
   display = loads(request.data) # display -> a 2d array of all pixel values
 
   # do the prediction logic here
   # the function should return the prediction made by the algorithm
   
+  prediction = np.argmax(model.predict([display]))
 
-  return str(randint(0, 9))
+  return str(prediction)
 
 if __name__ == "__main__":
   app.run(port=5000)
