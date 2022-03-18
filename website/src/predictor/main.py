ec1ef521ccd7edc9af0ee1b3afbecda3323c9810
Adam <monsieurfencer@gmail.com>
2022-03-18 11:51:47 -0400

    corrected for orientation
    same transforms needed as before

--- website/src/predictor/main.py
+++ website/src/predictor/main.py
@@ -3,24 +3,24 @@ from json import loads
 import numpy as np
 import tensorflow as tf
 import matplotlib
+import numpy
+
+model = None
+arr = None
 
 app = Flask(__name__)
 
 @app.route("/predict", methods=["GET", "POST"])
 def predict():
   data = loads(request.data)
-
+  arr = np.zeros((28, 28), dtype=int)
+  # print(data)
+  
   settings = data["settings"] # settings is an int between 0-3
   display = data["display"] # display is the 2d array from before
-
-  # do the prediction logic here
-  # the function should return the prediction made by the algorithm
-  for x in range(len(display)):
-    for y in range(len(display[x])):
-        spaces = 3 - len(str(display[x][y]))
-        print(display[x][y], end=' ' * spaces )
-
-  prediction = np.argmax(model.predict([display]))
+  arr = np.flip(np.rot90(np.array(display), -1), 1)
+  arr = arr.tolist() 
+  prediction = np.argmax(model.predict([arr]))
 
   return str(prediction)
 
