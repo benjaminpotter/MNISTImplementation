6be2c218bb41d660abe732e8daa3e68a7773411d
Adam <monsieurfencer@gmail.com>
2022-03-19 15:00:41 -0400

    added setting functionality
    KNN gitignored because too big

--- website/src/predictor/main.py
+++ website/src/predictor/main.py
@@ -4,9 +4,12 @@ import numpy as np
 import tensorflow as tf
 import matplotlib
 import numpy
+import pickle
+import sklearn
 
-model = None
-arr = None
+SVM = None
+CNN = None
+KNN = None
 
 app = Flask(__name__)
 
@@ -20,10 +23,27 @@ def predict():
   display = data["display"] # display is the 2d array from before
   arr = np.flip(np.rot90(np.array(display), -1), 1)
   arr = arr.tolist() 
-  prediction = np.argmax(model.predict([arr]))
+  
+  if settings == 0:
+    prediction = np.argmax(CNN.predict([arr]))
+  if settings == 1:
+    prediction = np.argmax(KNN.predict([arr]))
+  if settings == 2:
+    prediction = np.argmax(SVM.predict([arr]))
+  if settings == 3:
+    prediction1 = np.argmax(CNN.predict([arr]))
+    prediction2 = np.argmax(KNN.predict([arr]))
+    prediction3 = np.argmax(SVM.predict([arr]))
+    lst = [prediction1, prediction2, prediction3]
+    print(lst)
+
+    prediction = max(lst, key=lst.count)
+
 
   return str(prediction)
 
 if __name__ == "__main__":
-  model = tf.keras.models.load_model('./digitClassifier.model')
+  SVM = tf.keras.models.load_model('./digitClassifier.model')
+  CNN = tf.keras.models.load_model('CNN')
+  KNN = pickle.load(open('KNN', 'rb'))
   app.run(port=5000)
